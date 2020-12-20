12c646002fd231e6dfdc5c9311a884f2640d4b3b
aklepinin <alex@k8n.ru>
2020-12-20 18:48:31 +0500

    Yield iterable has been implemented

--- AoC-2020/src/common/queries/QueryTest.java
+++ AoC-2020/src/common/queries/QueryTest.java
@@ -1,6 +1,6 @@
 package common.queries;
 
-import java.util.Arrays;
+import common.queries.YieldIterable.Code;
 
 public class QueryTest
 {
@@ -10,9 +10,56 @@ public class QueryTest
 //        {
 //            System.out.println(s);
 //        }
-        for (Integer s : Query.range(10, 50).take(5))
+//        for (Integer s : Query.range(10, 50).take(5))
+//        {
+//            System.out.println(s);
+//        }
+//        for (Integer s : new IntGenerator())
+//        {
+//            System.out.println(s);
+//        }
+        for (Integer s : generateDivisors(120))
         {
             System.out.println(s);
+        }
+    }
+    
+    public static Iterable<Integer> generateDivisors(int value)
+    {
+        return YieldIterable.withYield((ctx) ->
+        {
+            int upBound = value / 2;
+            for (int i = 2; i <= upBound; i++)
+            {
+                if (value % i == 0)
+                    ctx.yield(i);
+            }
+        });
+    }
+
+    public static Iterable<Integer> generateDivisors1(int value)
+    {
+        return new YieldIterable<Integer>(){
+            protected void generate()
+            {
+                int upBound = value / 2;
+                for (int i = 2; i <= upBound; i++)
+                {
+                    if (value % i == 0)
+                        yield(i);
+                }
+            }
+        };
+    }
+    
+    public static class IntGenerator extends YieldIterable<Integer>
+    {
+        protected void generate()
+        {
+            for (int i = 0; i < 20; i+=5)
+            {
+                yield(i);
+            }
         }
     }
 }
