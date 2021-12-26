f9b9305cd0f24711c0192c052f416f51ee0c2d25
aklepinin <aklepinin@skbkontur.ru>
2021-12-27 00:53:15 +0500

    Day 23 (solved)

--- AoC-2021/src/day23/Puzzle2.java
+++ AoC-2021/src/day23/Puzzle2.java
@@ -47,28 +47,28 @@ public class Puzzle2 extends PuzzleCommon
         result.addEdge(8, 2, groupA, 2);
         result.addEdge(8, 3, groupA, 2);
         
-        result.addEdge(12, 8, groupA, 1);
-        result.addEdge(13, 12, groupA, 1);
-        result.addEdge(14, 13, groupA, 1);
+        result.addEdge(9, 8, groupA, 1);
+        result.addEdge(10, 9, groupA, 1);
+        result.addEdge(11, 10, groupA, 1);
         
-        result.addEdge(9, 3, groupB, 2);
-        result.addEdge(9, 4, groupB, 2);
+        result.addEdge(12, 3, groupB, 2);
+        result.addEdge(12, 4, groupB, 2);
         
-        result.addEdge(15, 9, groupB, 1);
-        result.addEdge(16, 15, groupB, 1);
-        result.addEdge(17, 16, groupB, 1);
+        result.addEdge(13, 12, groupB, 1);
+        result.addEdge(14, 13, groupB, 1);
+        result.addEdge(15, 14, groupB, 1);
         
-        result.addEdge(10, 4, groupC, 2);
-        result.addEdge(10, 5, groupC, 2);
+        result.addEdge(16, 4, groupC, 2);
+        result.addEdge(16, 5, groupC, 2);
         
-        result.addEdge(18, 10, groupC, 1);
+        result.addEdge(17, 16, groupC, 1);
+        result.addEdge(18, 17, groupC, 1);
         result.addEdge(19, 18, groupC, 1);
-        result.addEdge(20, 19, groupC, 1);
         
-        result.addEdge(11, 5, groupD, 2);
-        result.addEdge(11, 6, groupD, 2);
+        result.addEdge(20, 5, groupD, 2);
+        result.addEdge(20, 6, groupD, 2);
         
-        result.addEdge(21, 11, groupD, 1);
+        result.addEdge(21, 20, groupD, 1);
         result.addEdge(22, 21, groupD, 1);
         result.addEdge(23, 22, groupD, 1);
         
@@ -93,22 +93,22 @@ public class Puzzle2 extends PuzzleCommon
         mapLocationToNode(Pair.of(1, 11), 7);
         
         mapLocationToNode(Pair.of(2, 3), 8);
-        mapLocationToNode(Pair.of(2, 5), 9);
-        mapLocationToNode(Pair.of(2, 7), 10);
-        mapLocationToNode(Pair.of(2, 9), 11);
+        mapLocationToNode(Pair.of(3, 3), 9);
+        mapLocationToNode(Pair.of(4, 3), 10);
+        mapLocationToNode(Pair.of(5, 3), 11);
+
+        mapLocationToNode(Pair.of(2, 5), 12);
+        mapLocationToNode(Pair.of(3, 5), 13);
+        mapLocationToNode(Pair.of(4, 5), 14);
+        mapLocationToNode(Pair.of(5, 5), 15);
         
-        mapLocationToNode(Pair.of(3, 3), 12);
-        mapLocationToNode(Pair.of(4, 3), 13);
-        mapLocationToNode(Pair.of(5, 3), 14);
+        mapLocationToNode(Pair.of(2, 7), 16);
+        mapLocationToNode(Pair.of(3, 7), 17);
+        mapLocationToNode(Pair.of(4, 7), 18);
+        mapLocationToNode(Pair.of(5, 7), 19);
+
         
-        mapLocationToNode(Pair.of(3, 5), 15);
-        mapLocationToNode(Pair.of(4, 5), 16);
-        mapLocationToNode(Pair.of(5, 5), 17);
-        
-        mapLocationToNode(Pair.of(3, 7), 18);
-        mapLocationToNode(Pair.of(4, 7), 19);
-        mapLocationToNode(Pair.of(5, 7), 20);
-        
+        mapLocationToNode(Pair.of(2, 9), 20);
         mapLocationToNode(Pair.of(3, 9), 21);
         mapLocationToNode(Pair.of(4, 9), 22);
         mapLocationToNode(Pair.of(5, 9), 23);
@@ -124,34 +124,34 @@ public class Puzzle2 extends PuzzleCommon
 
     public static Board2D fillValidStates()
     {
-        Board2D result = new Board2D(16, statesCount + 1);
+        Board2D result = new Board2D(statesCount + 1, 16);
         result.setAll(0);
-        for (var st : new int[] {8, 12, 13, 14})
+        for (var n : new int[] {0, 1, 2, 3})
         {
-            for (var n : new int[] {0, 1, 2, 3})
+            for (var st : new int[] {8, 9, 10, 11})
             {
-                result.setAtRC(st, n, 1);
+                result.setAtRC(n, st, 1);
             }
         }
-        for (var st : new int[] {9, 15, 16, 17})
+        for (var n : new int[] {4, 5, 6, 7})
         {
-            for (var n : new int[] {4, 5, 6, 7})
+            for (var st : new int[] {12, 13, 14, 15})
             {
-                result.setAtRC(st, n, 1);
+                result.setAtRC(n, st, 1);
             }
         }
-        for (var st : new int[] {10, 18, 19, 20})
+        for (var n : new int[] {8, 9, 10, 11})
         {
-            for (var n : new int[] {8, 9, 10, 11})
+            for (var st : new int[] {16, 17, 18, 19})
             {
-                result.setAtRC(st, n, 1);
+                result.setAtRC(n, st, 1);
             }
         }
-        for (var st : new int[] {11, 21, 22, 23})
+        for (var n : new int[] {12, 13, 14, 15})
         {
-            for (var n : new int[] {12, 13, 14, 15})
+            for (var st : new int[] {20, 21, 22, 23})
             {
-                result.setAtRC(st, n, 1);
+                result.setAtRC(n, st, 1);
             }
         }
         return result;
@@ -162,6 +162,7 @@ public class Puzzle2 extends PuzzleCommon
     {
         // 0-3 - A, 4-7 - B, 8-11 - C, 12-15 - D
         public int[] locations = new int[16];
+        public int[] moveCounts = new int[16];
         public long energy;
         public State from;
         
@@ -169,8 +170,139 @@ public class Puzzle2 extends PuzzleCommon
         {
             for (int i = 0; i < locations.length; i++)
             {
-                if (validStates.getAtRC(locations[i], i) == 0)
+                if (validStates.getAtRC(i, locations[i]) == 0)
                     return false;
+            }
+            return true;
+        }
+        
+        public boolean shouldConsiderTransition(State from, State to, int byPos)
+        {
+            if (from.locations[byPos] == to.locations[byPos])
+            {
+                return false;
+            }
+            if ((from.locations[byPos] <= 7) && (to.locations[byPos] <= 7))
+            {
+                return false;
+            }
+            if (from.moveCounts[byPos] >= 2)
+            {
+                return false;
+            }
+            
+            // TODO: remove
+//            if (byPos >= 8)
+//                return false;
+            
+            if (to.locations[byPos] > 7)    // enters home
+            {
+                var pos = to.locations[byPos];
+                int upBound;
+
+                // A-s home
+                upBound = 12;
+                if (pos >= upBound - 4 && pos < upBound) // 8-11
+                {
+                    var found = 0;
+                    for (var i = 0; i < to.locations.length; i++)
+                    {
+                        var loc = to.locations[i];
+                        if (loc >= pos && loc < upBound)
+                        {
+                            if (i >= 0 && i < 4)    // A
+                            {
+                                found++;
+                            }
+                            else
+                            {
+                                return false;    // other type is below
+                            }
+                        }
+                    }
+                    if (pos + found < upBound)
+                    {
+                        return false; // empty space below
+                    }
+                }
+
+                // B-s home
+                upBound = 16;
+                if (pos >= upBound - 4 && pos < upBound) // 12-15
+                {
+                    var found = 0;
+                    for (var i = 0; i < to.locations.length; i++)
+                    {
+                        var loc = to.locations[i];
+                        if (loc >= pos && loc < upBound)
+                        {
+                            if (i >= 4 && i < 8)    // B
+                            {
+                                found++;
+                            }
+                            else
+                            {
+                                return false;    // other type is below
+                            }
+                        }
+                    }
+                    if (pos + found < upBound)
+                    {
+                        return false; // empty space below
+                    }
+                }
+
+                // C-s home
+                upBound = 20;
+                if (pos >= upBound - 4 && pos < upBound) // 16-19
+                {
+                    var found = 0;
+                    for (var i = 0; i < to.locations.length; i++)
+                    {
+                        var loc = to.locations[i];
+                        if (loc >= pos && loc < upBound)
+                        {
+                            if (i >= 8 && i < 12)    // C
+                            {
+                                found++;
+                            }
+                            else
+                            {
+                                return false;    // other type is below
+                            }
+                        }
+                    }
+                    if (pos + found < upBound)
+                    {
+                        return false; // empty space below
+                    }
+                }
+
+                // D-s home
+                upBound = 24;
+                if (pos >= upBound - 4 && pos < upBound) // 20-23
+                {
+                    var found = 0;
+                    for (var i = 0; i < to.locations.length; i++)
+                    {
+                        var loc = to.locations[i];
+                        if (loc >= pos && loc < upBound)
+                        {
+                            if (i >= 12 && i < 16)    // D
+                            {
+                                found++;
+                            }
+                            else
+                            {
+                                return false;    // other type is below
+                            }
+                        }
+                    }
+                    if (pos + found < upBound)
+                    {
+                        return false; // empty space below
+                    }
+                }
             }
             return true;
         }
@@ -182,6 +314,10 @@ public class Puzzle2 extends PuzzleCommon
             {
                 busyLocations[locations[i]] = true;
             }
+//            System.out.println("=======================");
+//            printState(this);
+//            System.out.println("-----------------------");
+            
             ArrayList<State> nextStates = new ArrayList<>();
             for (var i = 0; i < locations.length; i++)
             {
@@ -196,9 +332,10 @@ public class Puzzle2 extends PuzzleCommon
                     var current = q.poll();
                     if (visited.add(current))
                     {
-                        if (current != this)
+                        if (shouldConsiderTransition(this, current, i))
                         {
                             nextStates.add(current);
+//                            printState(current);
                         }
                         for (var nextInfo : gr.outboundEdges(current.locations[i]))
                         {
@@ -208,12 +345,11 @@ public class Puzzle2 extends PuzzleCommon
                                 var s = new State();
                                 s.locations = current.locations.clone();
                                 s.locations[i] = loc;
+                                s.moveCounts = this.moveCounts.clone();
+                                s.moveCounts[i]++;
                                 s.energy = current.energy + (decodeIdxEnergy(i) * nextInfo.getValue2().intValue());
                                 s.from = this;
-                                if (!visited.contains(s))
-                                {
-                                    nextStates.add(s);
-                                }
+                                q.add(s);
                             }
                         }
                     }
@@ -331,22 +467,13 @@ public class Puzzle2 extends PuzzleCommon
     public void solve()
         throws Exception
     {
-//        ArrayList<LinesGroup> groups = readAllLineGroups("input1.txt");
-//        // System.out.println(groups.size());
-//        
-//        int result = 0;
-//        for (LinesGroup group : groups)
-//        {
-//            result += group.processGroup(this::processGroup);
-//        }
-//        System.out.println(result);
-        
 //        ArrayList<String> lines = readAllLines("input1.txt");
         
-//        ArrayList<String> lines = readAllLinesNonEmpty("input1_1.txt", false);
-        ArrayList<String> lines = readAllLinesNonEmpty("input4.txt", false);
+        ArrayList<String> lines = readAllLinesNonEmpty("input1_1.txt", false);
+//        ArrayList<String> lines = readAllLinesNonEmpty("input4.txt", false);
 //        ArrayList<String> lines = readAllLinesNonEmpty("input3.txt", false);
 //        ArrayList<String> lines = readAllLinesNonEmpty("input2.txt", false);
+//        ArrayList<String> lines = readAllLinesNonEmpty("input5.txt", false);
         
         Board2D board = new Board2D(13, 7);
         for (int i = 0; i < 7; i++)
@@ -373,7 +500,9 @@ public class Puzzle2 extends PuzzleCommon
             visitedStates.put(state, state.energy);
             if (state.isFinal())
             {
-                System.out.println("Found!!!");
+                System.out.println();
+                System.out.println("!!!Found!!!");
+                System.out.println();
                 printState(state);
                 
                 var prev = state;
