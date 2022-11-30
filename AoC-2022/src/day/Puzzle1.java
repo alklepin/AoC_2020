6d7d010ae9cf521c7663667eda7cf793a50fd597
aklepinin <alex@k8n.ru>
2022-11-30 22:38:46 +0500

    Preparations for AoC 2022

---
+++ AoC-2022/src/day/Puzzle1.java
@@ -0,0 +1,68 @@
+package day;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import common.PuzzleCommon;
+
+public class Puzzle1 extends PuzzleCommon
+{
+
+    public static void main(String [] args)
+        throws Exception
+    {
+        new Puzzle1().solve();
+    }
+    
+    public int processGroup(LinesGroup group)
+    {
+        HashMap<Character, Integer> chars = new HashMap<>();
+        for (String line : group)
+        {
+            for (int i = 0; i < line.length(); i++)
+            {
+                char key = line.charAt(i);
+                int count = 0;
+                if (chars.get(key) != null)
+                {
+                    count = chars.get(key);
+                }
+                chars.put(key, count + 1);
+            }
+        }
+        int count = 0;
+        int groupSize = group.size();
+        for (Integer v : chars.values())
+        {
+            if (v == groupSize)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+    
+    public void solve()
+        throws Exception
+    {
+//        ArrayList<LinesGroup> groups = readAllLineGroups("input1.txt");
+//        // System.out.println(groups.size());
+//        
+//        int result = 0;
+//        for (LinesGroup group : groups)
+//        {
+//            result += group.processGroup(this::processGroup);
+//        }
+//        System.out.println(result);
+        
+//        ArrayList<String> lines = readAllLines("input1.txt");
+        
+//        ArrayList<String> lines = readAllLinesNonEmpty("input1.txt");
+//        int result = 0;
+//        for (String line : lines)
+//        {
+//        }
+//        System.out.println(result);
+        
+    }
+}
