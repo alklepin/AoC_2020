4ec43a97d46028935105dc7dec0b3325be322062
aklepinin <alex@k8n.ru>
2022-12-17 14:05:50 +0500

    Day 17

--- AoC-2022/src/day17/Puzzle1_optimized.java
+++ AoC-2022/src/day17/Puzzle1_optimized.java
@@ -1,6 +1,5 @@
 package day17;
 
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 
@@ -9,14 +8,15 @@ import common.PuzzleCommon;
 import common.boards.Board2D;
 import common.boards.IntPair;
 import common.boards.Pair;
+import day17.Puzzle2.Shape;
 
-public class Puzzle2 extends PuzzleCommon
+public class Puzzle1_optimized extends PuzzleCommon
 {
 
     public static void main(String [] args)
         throws Exception
     {
-        new Puzzle2().solve();
+        new Puzzle1_optimized().solve();
     }
     
     public void solve()
@@ -33,14 +33,9 @@ public class Puzzle2 extends PuzzleCommon
         int shapeIdx = 0;
         int commandIdx = 0;
         HashSet<IntPair> chamber = new HashSet<>();
-//        var expectedLoopLength = commands.length * Shape.SHAPES.length * 2000;
-        var expectedLoopLength = 8705;
-        int[] heightAtAtep = new int[expectedLoopLength];
-        HashMap<Integer, Integer> rows = new HashMap<>(); 
-        HashMap<Long, Integer> savedStates = new HashMap<>();
-        int lastStep = 0;
-        int lastHeight = 0;
-        for (int step = 0; step < expectedLoopLength; step++)
+        var expectedLoopLength = commands.length * Shape.SHAPES.length * 200;
+        int[] rows = new int[expectedLoopLength]; 
+        for (int step = 0; step < 2022; step++)
         {
 //            System.out.println("Step: "+step);
             var rock = Shape.SHAPES[shapeIdx];
@@ -89,18 +84,12 @@ public class Puzzle2 extends PuzzleCommon
                 var newPoint = p.add(rockPos);
                 chamber.add(newPoint);
                 height = Math.max(height, newPoint.getY()+1);
-                var rowObj = rows.get(newPoint.getY());
-                var row = rowObj != null ? rowObj.intValue() : 0;
-                row |= 1 << newPoint.getX();
-                rows.put(newPoint.getY(), row);
-                if (row == 127)
+                rows[newPoint.getY()] |= 1 << newPoint.getX();
+                if (rows[newPoint.getY()] == 127)
                 {
                     truncateAt = Math.max(truncateAt,newPoint.getY()-1);
                 }
             }
-            shapeIdx = (shapeIdx + 1) % Shape.SHAPES.length;
-            heightAtAtep[step] = height;
-            
             if (truncateAt > 0)
             {
                 Iterator<IntPair> it = chamber.iterator();
@@ -110,95 +99,17 @@ public class Puzzle2 extends PuzzleCommon
                     if (p.getY() < truncateAt)
                         it.remove();
                 }
-
-                Iterator<Integer> it1 = rows.keySet().iterator();
-                while (it1.hasNext())
-                {
-                    var p = it1.next();
-                    if (p < truncateAt - 4)
-                        it1.remove();
-                }
             }
-
-            if (commandIdx == 109 && shapeIdx == 0)
-//              if ((commandIdx >= 100) && (commandIdx <= 110))
-              {
-                  var state = encodeState(rows, height);
-                  var knownStep = savedStates.get(state);
-                  if (knownStep != null)
-                  {
-                      System.out.println("Loop step: "+ knownStep + " global step: "+step);
-                      System.out.println("size: " + savedStates.size() + " add state: " + state + " commandIdx: " + commandIdx + " shape: " + shapeIdx);
-                      System.out.println("step diff: " + (step-lastStep) + " height diff: " + (height - lastHeight));
-                      System.out.println("height: " + heightAtAtep[step]);
-                      lastHeight = height;
-                      lastStep = step;
-                  }
-                  else
-                  {
-                      savedStates.put(state, step);
-                  }
-              }
-              
+            
+            shapeIdx = (shapeIdx + 1) % Shape.SHAPES.length;
             
 //            print(chamber, height);
         }
-
-//        int[] line = new int[expectedLoopLength];
-//        for (var p : chamber)
-//        {
-//            if (p.getY() < expectedLoopLength)
-//                line[p.getY()] |= 1 << p.getX();
-//        }
-////        int delta = 13405;
-//        int delta = 161456000;
-//        for (var idx = 2; idx < expectedLoopLength; idx++)
-//        {
-//            while (idx + delta < expectedLoopLength && line[idx] == line[idx+delta])
-//                idx++;
-//            if (idx + delta < expectedLoopLength)
-//                delta++;
-//        }
-//        
-//        System.out.println("Delta:" + delta);
-
-//        for (int idx = 0; idx < 2 * delta; idx++)
-//        for (int idx = 0; idx < 200; idx++)
-//        {
-//            System.out.println(heightAtAtep[idx+delta] - heightAtAtep[idx]);
-//        }
         
+        System.out.println(height);
         
-//        Loop step: 1745 global step: 5225
-//        size: 1 add state: 30380064 commandIdx: 109 shape: 0
-//        step diff: 1740 height diff: 2681
-        
-        var stepNeeded = 1000000000000l - 1;
-        var baseStep = 6964;
-        var s1 = stepNeeded - baseStep;
-        var loopSize = 1740;
-        long loopCount = s1 / loopSize;
-        long loopedHeight = loopCount * 2681;
-        var sourceStep = (int)(stepNeeded - loopCount * loopSize);
-        var result = loopedHeight + heightAtAtep[sourceStep]; 
-        
-        System.out.println(result);
     }
     
-    private long encodeState(int [] rows, int height)
-    {
-        return (long)rows[height-1] + rows[height-2] << 7 + rows[height-3] << 14 + rows[height-4] << 21;
-    }
-    
-    private long encodeState(HashMap<Integer, Integer> rows, int height)
-    {
-        var result = (long)rows.get(height-1).longValue() 
-            + (rows.get(height-2).intValue() << 7) 
-            + (rows.get(height-3).intValue() << 14) 
-            + (rows.get(height-4).intValue() << 21);
-        return result;
-    }
-
     private static void print(HashSet<IntPair> chamber, int height)
     {
         Board2D board = new Board2D(9, height+1);
