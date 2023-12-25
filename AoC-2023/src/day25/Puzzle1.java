93d44a75be5e0ffb3d2e751c432aa910eafcfd4b
aklepinin <alex@k8n.ru>
2023-12-26 00:01:06 +0500

    Day 25

--- AoC-2023/src/day25/Puzzle1.java
+++ AoC-2023/src/day25/Puzzle1.java
@@ -1,7 +1,13 @@
 package day25;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
 import common.LinesGroup;
 import common.PuzzleCommon;
+import common.graph.ImplicitGraph;
+import common.queries.Query;
 
 public class Puzzle1 extends PuzzleCommon
 {
@@ -29,12 +35,141 @@ public class Puzzle1 extends PuzzleCommon
         
 //        LinesGroup lines = readAllLines(inputFile);
         
-//        LinesGroup lines = readAllLinesNonEmpty(inputFile);
-//        int result = 0;
-//        for (String line : lines)
-//        {
-//        }
-//        System.out.println(result);
+        LinesGroup lines = readAllLinesNonEmpty(inputFile);
+        int result = 0;
+        HashSet<Edge> edgesSet = new HashSet<>();
+        ArrayList<Edge> edgesList = new ArrayList<>();
+        HashMap<String, ArrayList<String>> edges = new HashMap<>();
+        
+        for (String line : lines)
+        {
+            var parts = line.split(":");
+            var from = parts[0].trim();
+            var toParts = parts[1].trim().split("\\s+");
+            for (var t : toParts)
+            {
+                var edge = new Edge(from, t);
+                edgesSet.add(edge);
+                edgesList.add(edge);
+                var listFrom = edges.get(from);
+                if (listFrom == null)
+                {
+                    listFrom = new ArrayList<String>();
+                    edges.put(from, listFrom);
+                }
+                listFrom.add(t);
+                var listTo = edges.get(t);
+                if (listTo == null)
+                {
+                    listTo = new ArrayList<String>();
+                    edges.put(t, listTo);
+                }
+                listTo.add(from);
+            }
+        }
+        //var start = edgesList.get(0).node1;
+        var nodes = Query.wrap(edges.keySet()).toList();
+        System.out.println("Nodes count:"+nodes.size());
+        
+        loop:
+        for (var idx1 = 0; idx1 < edgesList.size(); idx1++)
+        {
+            System.out.println("Loop index:"+idx1);
+            var edge1 = edgesList.get(idx1);
+//            var edge1 = new Edge("hfx","pzl");
+            for (var idx2 = idx1 + 1; idx2 < edgesList.size(); idx2++)
+            {
+                var edge2 = edgesList.get(idx2);
+//                var edge2 = new Edge("bvb","cmg");
+                var res = ImplicitGraph.StrongComponentsFinder(nodes, node ->
+                {
+                    var list = Query.wrap(edges.get(node))
+                    .where(next -> {
+                        var e = new Edge(node, next);
+                        return !edge1.equals(e) && !edge2.equals(e);
+                    }).toList();
+//                    for (var n1 : list)
+//                    {
+//                        System.out.println("from: "+node+" to: "+n1);
+//                    }
+                    
+                    return list;
+                });
+                if (res.getBridges().size() == 1) //  && res.getStrongComponents().size() >= 2
+                {
+                    System.out.println(res.getBridges().get(0));
+                    System.out.println(edge1);
+                    System.out.println(edge2);
+                    System.out.println(res.getStrongComponents().get(0).size());
+                    System.out.println(res.getStrongComponents().get(1).size());
+                    
+                    break loop;
+                }
+            }
+        }
+        System.out.println(result);
+    }
+    
+    public static class Edge
+    {
+        String node1;
+        String node2;
+        
+        public Edge(String n1, String n2)
+        {
+            if (n1.compareTo(n2) > 0)
+            {
+                var tmp = n1;
+                n1 = n2;
+                n2 = tmp;
+            }
+            node1 = n1;
+            node2 = n2;
+        }
+
+        @Override
+        public int hashCode()
+        {
+            final int prime = 31;
+            int result = 1;
+            result = prime * result + ((node1 == null) ? 0 : node1.hashCode());
+            result = prime * result + ((node2 == null) ? 0 : node2.hashCode());
+            return result;
+        }
+
+        @Override
+        public boolean equals(Object obj)
+        {
+            if (this == obj)
+                return true;
+            if (obj == null)
+                return false;
+            if (getClass() != obj.getClass())
+                return false;
+            Edge other = (Edge)obj;
+            if (node1 == null)
+            {
+                if (other.node1 != null)
+                    return false;
+            }
+            else if (!node1.equals(other.node1))
+                return false;
+            if (node2 == null)
+            {
+                if (other.node2 != null)
+                    return false;
+            }
+            else if (!node2.equals(other.node2))
+                return false;
+            return true;
+        }
+
+        @Override
+        public String toString()
+        {
+            return "Edge [node1=" + node1 + ", node2=" + node2 + "]";
+        }
+        
         
     }
 }
--- AoC-common/src/common/graph/ImplicitGraph.java
+++ AoC-common/src/common/graph/ImplicitGraph.java
@@ -74,7 +74,22 @@ public class ImplicitGraph
             return visitedFrom.keySet();
         }
     }
-    
+
+    public static class SearchResultDFS<TNode> extends SearchResult<TNode>
+    {
+
+        public SearchResultDFS(Iterable<TNode> starts, TNode end,
+            HashMap<TNode, TNode> visitedFrom)
+        {
+            super(starts, end, visitedFrom);
+        }
+
+        public SearchResultDFS(TNode start, TNode end, HashMap<TNode, TNode> visitedFrom)
+        {
+            super(start, end, visitedFrom);
+        }
+
+    }
 //    public static <TNode> SearchResult<TNode> BFS(TNode start, TNode end, MoveGenerator<TNode> moveGenerator)
 //    {
 //        LinkedList<TNode> queue = new LinkedList<>();
@@ -198,7 +213,7 @@ public class ImplicitGraph
             {
                 if (visitedFrom.containsKey(next))
                     continue;
-
+                
                 reachedFrom.put(next, current);
                 if (end != null && next.equals(end))
                 {
@@ -209,6 +224,189 @@ public class ImplicitGraph
         }
         visitedFrom.remove(start);
         return new SearchResult<TNode>(start, end, visitedFrom);
+    }
+
+    private static class Edge<TNode>
+    {
+        TNode n1;
+        TNode n2;
+
+        public static <T> Edge<T> of(T node1, T node2)
+        {
+            return new Edge<T>(node1, node2);
+        }
+        
+        public Edge(TNode n1, TNode n2)
+        {
+            this.n1 = n1;
+            this.n2 = n2;
+        }
+        
+        @Override
+        public int hashCode()
+        {
+            final int prime = 31;
+            int result = 1;
+            result = prime * result + ((n1 == null) ? 0 : n1.hashCode());
+            result = prime * result + ((n2 == null) ? 0 : n2.hashCode());
+            return result;
+        }
+        @Override
+        public boolean equals(Object obj)
+        {
+            if (this == obj)
+                return true;
+            if (obj == null)
+                return false;
+            if (getClass() != obj.getClass())
+                return false;
+            @SuppressWarnings("unchecked")
+            Edge<TNode> other = (Edge<TNode>)obj;
+            if (n1 == null)
+            {
+                if (other.n1 != null)
+                    return false;
+            }
+            else if (!n1.equals(other.n1))
+                return false;
+            if (n2 == null)
+            {
+                if (other.n2 != null)
+                    return false;
+            }
+            else if (!n2.equals(other.n2))
+                return false;
+            return true;
+        }
+
+        @Override
+        public String toString()
+        {
+            return "Edge [n1=" + n1 + ", n2=" + n2 + "]";
+        }
+    }
+    
+    private static class NodeInfo<TNode>
+    {
+        int index;
+        int lowLink;
+        boolean onStack;
+    }
+    
+    private static class StrongComponentsFinderState<TNode>
+    {
+        HashMap<TNode, NodeInfo<TNode>> nodeInfos = new HashMap<>();
+        HashMap<TNode, TNode> visitedFrom = new HashMap<>();
+        ArrayList<Edge<TNode>> bridges = new ArrayList<>();
+        Stack<TNode> stack = new Stack<>();
+        ArrayList<ArrayList<TNode>> strongComponents = new ArrayList<>();
+        
+        MoveGenerator<TNode> moveGenerator;
+        
+        public StrongComponentsFinderState(MoveGenerator<TNode> moveGenerator)
+        {
+            this.moveGenerator = moveGenerator;
+        }
+    }
+    
+    public static class StrongComponentsSearchResult<TNode>
+    {
+
+        private final ArrayList<ArrayList<TNode>> m_strongComponents;
+        private final ArrayList<Edge<TNode>> m_bridges;
+
+        public StrongComponentsSearchResult(
+            ArrayList<ArrayList<TNode>> strongComponents,
+            ArrayList<Edge<TNode>> bridges)
+        {
+            m_strongComponents = strongComponents;
+            m_bridges = bridges;
+        }
+
+        public ArrayList<ArrayList<TNode>> getStrongComponents()
+        {
+            return m_strongComponents;
+        }
+
+        public ArrayList<Edge<TNode>> getBridges()
+        {
+            return m_bridges;
+        }
+    }
+    
+//    public static <TNode> StrongComponentsSearchResult<TNode> StrongComponentsFinder(TNode start, MoveGenerator<TNode> moveGenerator)
+//    {
+//        return StrongComponentsFinder(Query.wrap(start), moveGenerator);
+//    }
+
+    public static <TNode> StrongComponentsSearchResult<TNode> StrongComponentsFinder(Iterable<TNode> nodes, MoveGenerator<TNode> moveGenerator)
+    {
+        var state = new StrongComponentsFinderState<TNode>(moveGenerator);
+        for (var start : nodes)
+        {
+            if (state.nodeInfos.get(start) == null)
+            {
+                state.visitedFrom.put(start, start);
+                StrongComponentsFinderImpl(start, state);
+            }
+        }
+        return new StrongComponentsSearchResult<TNode>(state.strongComponents, state.bridges);
+    }
+    
+    public static <TNode> void StrongComponentsFinderImpl(TNode current, StrongComponentsFinderState<TNode> state)
+    {
+        var nodeInfo = new NodeInfo<TNode>();
+        state.nodeInfos.put(current, nodeInfo);
+        nodeInfo.index = state.nodeInfos.size();
+        nodeInfo.lowLink = nodeInfo.index;
+        nodeInfo.onStack = true;
+        state.stack.push(current);
+
+        // System.out.println("Look at: " + current + " index: "+ nodeInfo.index);
+        var a = 1;
+        
+        for (var next : state.moveGenerator.nextNodes(current))
+        {
+            if (next.equals(state.visitedFrom.get(current)))
+                continue;
+            
+            var nextNodeInfo = state.nodeInfos.get(next);
+            if (nextNodeInfo == null)
+            {
+                state.visitedFrom.put(next, current);
+                StrongComponentsFinderImpl(next, state);
+                nextNodeInfo = state.nodeInfos.get(next);
+                nodeInfo.lowLink = Math.min(nodeInfo.lowLink, nextNodeInfo.lowLink);
+            }
+            else if (nextNodeInfo.onStack)
+            {
+                // Вершина w находится в стеке, значит, принадлежит той же компоненте сильной связности, что и v
+                // Если w не в стеке, значит, ребро (v, w) ведёт в ранее обработанную компоненту сильной связности и должна быть проигнорирована
+                // Замечание: в следующей строке намеренно используется w.index вместо w.lowlink - это отсылает к исходной статье Тарьяна
+                // Если заменить w.index на w.lowlink, алгоритм останется корректным
+                nodeInfo.lowLink = Math.min(nodeInfo.lowLink, nextNodeInfo.index);
+            }
+        }
+
+//         System.out.println("Look at: " + current + " index: "+ nodeInfo.index + " low index: " + nodeInfo.lowLink);
+        
+        if (nodeInfo.lowLink == nodeInfo.index)
+        {
+            ArrayList<TNode> strongComponent = new ArrayList<>();
+            TNode node;
+            do
+            {
+              node = state.stack.pop();
+              var nInfo = state.nodeInfos.get(node);
+              nInfo.onStack = false;
+              strongComponent.add(node);
+            } while (!current.equals(node));
+            state.strongComponents.add(strongComponent);
+
+            var prev = state.visitedFrom.get(current);
+            if (!current.equals(prev))
+                state.bridges.add(Edge.of(current, prev));
+        }
     }
 
     //===========================================================================
