60aeb18cc5bfc3db5d97aa879a5ed99199ec71ff
aklepinin <alex@k8n.ru>
2024-12-01 02:12:53 +0500

    New season (December 2024)

---
+++ AoC-2024/src/day/Puzzle1.java
@@ -0,0 +1,40 @@
+package day;
+
+import common.LinesGroup;
+import common.PuzzleCommon;
+
+public class Puzzle1 extends PuzzleCommon
+{
+
+    public static void main(String [] args)
+        throws Exception
+    {
+        var start = System.currentTimeMillis();
+        try
+        {
+            new Puzzle1().solve();
+        }
+        finally
+        {
+            var end = System.currentTimeMillis();
+            System.out.printf("Time spent: %f sec\n", (end - start) / 1000.0);
+        }
+    }
+    
+    public void solve()
+        throws Exception
+    {
+        var inputFile = "input1.txt";
+//        var inputFile = "input1_test.txt";
+        
+//        LinesGroup lines = readAllLines(inputFile);
+        
+//        LinesGroup lines = readAllLinesNonEmpty(inputFile);
+//        int result = 0;
+//        for (String line : lines)
+//        {
+//        }
+//        System.out.println(result);
+        
+    }
+}
