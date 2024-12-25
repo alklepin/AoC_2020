207cf8d4c8c53b3cf7dce324de73aed701cab371
aklepinin <alex@k8n.ru>
2024-12-25 10:24:07 +0500

    Day 25

--- AoC-2024/src/day25/Puzzle1.java
+++ AoC-2024/src/day25/Puzzle1.java
@@ -1,7 +1,10 @@
 package day25;
 
-import common.LinesGroup;
+import java.util.ArrayList;
+
 import common.PuzzleCommon;
+import common.boards.Board2D;
+import common.boards.IntPair;
 
 public class Puzzle1 extends PuzzleCommon
 {
@@ -26,15 +29,51 @@ public class Puzzle1 extends PuzzleCommon
     {
         var inputFile = "input1.txt";
 //        var inputFile = "input1_test.txt";
+        var lineGroups = readAllLineGroups(inputFile); 
+
         
-//        LinesGroup lines = readAllLines(inputFile);
+        var keys = new ArrayList<ArrayList<Integer>>();
+        var locks = new ArrayList<ArrayList<Integer>>();
         
-//        LinesGroup lines = readAllLinesNonEmpty(inputFile);
-//        int result = 0;
-//        for (String line : lines)
-//        {
-//        }
-//        System.out.println(result);
+        for (var group : lineGroups)
+        {
+            var isKey = group.line(0).contains(".");
+            var board = Board2D.parseAsCharsXY(group);
+            var data = new ArrayList<Integer>();
+            for (var idx = 0; idx < board.getWidth(); idx++)
+            {
+                var sharpCount = board.rayXY(IntPair.of(idx, 0), IntPair.UP)
+                    .where(cell -> board.getCharAtXY(cell) == '#').count();
+                if (isKey)
+                    sharpCount--;
+                data.add(sharpCount);
+            }
+            if (isKey)
+                keys.add(data);
+            else
+                locks.add(data);
+        }
+        
+        
+        int result = 0;
+        for (var lock : locks)
+        {
+            for (var key : keys)
+            {
+                var overlaps = false;
+                for (var idx = 0; idx < key.size(); idx++)
+                {
+                    if (lock.get(idx) + key.get(idx) > 5)
+                    {
+                        overlaps = true;
+                        break;
+                    }
+                }
+                if (!overlaps)
+                    result++;
+            }
+        }
+        System.out.println(result);
         
     }
 }
